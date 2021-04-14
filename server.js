6c4fa3d9cdf5efe180a38685397ba79ffe44e58b
Naeem <naeem97ali@gmail.com>
2021-04-15 00:05:04 +0500

    separating our cra app into client folder, added express to help handle backend stripe payment

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require("path");
 
 if (process.env.NODE_ENV !== "production") require("dotenv").config();
 
-const stripe = require("stripe")("sk_test_tsk8RrcXK8YQ49lfRHtE7kpj");
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 const app = express();
 const port = process.env.PORT || 5000;
